4e957b84de3427ea6d09d0f38ff8279e44d87a53
s111310532 <s111310532@student.nqu.edu.tw>
2026-01-04 12:02:20 +0800

    Create Pong.asm

---
+++ 06/Pong.asm
@@ -0,0 +1,12 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/6/pong/Pong.asm
+
+// This Pong game code was originally written in the high-level Jack language.
+// The Jack code was then translated by the Jack compiler into VM code.
+// The VM code was then translated by the VM translator into the Hack
+// assembly code shown here. The variable and label symbols were generated
+// automatically by the translators. This information is given for context only.
+// Jack, the Jack compiler, and the VM translator are covered in Part II
+// of the course, and are irrelevant for project 6
