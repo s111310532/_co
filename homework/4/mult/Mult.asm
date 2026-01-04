e4b7557f2343aeba08b17f4b490e4b211e692fc4
s111310532 <s111310532@student.nqu.edu.tw>
2026-01-04 11:11:49 +0800

    Update Mult.asm

--- homework/4/mult/Mult.asm
+++ homework/4/mult/Mult.asm
@@ -8,3 +8,42 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+// Mult.asm
+// R2 = R0 * R1
+// 使用重複加法：將 R0 加到 R2，共執行 R1 次。
+
+    // 初始化 R2 為 0
+    @R2
+    M=0
+
+    // 初始化計數器 i = R1
+    @R1
+    D=M
+    @i
+    M=D
+
+(LOOP)
+    // 檢查計數器 i 是否為 0，如果是則跳轉到 END
+    @i
+    D=M
+    @END
+    D;JLE    // if i <= 0 goto END
+
+    // 執行加法：R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // 計數器遞減：i = i - 1
+    @i
+    M=M-1
+
+    // 回到迴圈起點
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP    // 無限迴圈結束程式
+
